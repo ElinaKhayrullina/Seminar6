33e554a58344078bcabc3259c1c1f68f1bcd2185
Diana Bayazitova <onlydi@mail.ru>
2022-09-09 20:48:58 +0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное
+/*
+void Dec (int [] array){
+    int temp=0;
+    int j=1;
+
+    for (int i=0;i<array.Length;i++){
+        int[] number=new int [5];
+            for (int z=0;z<number.Length;z++) {
+                number[z]=2;
+                Console.Write(number[z]+" ");
+
+ }
+ 
+        temp=array[i]%2;
+       int n=array[i]/2;
+        number[i]=temp;
+            while (n!=0){
+                temp=n%2;
+                n=n/2;
+                number[i+j]=temp;
+                j++;
+
+            }
+             for (int k=number.Length;k>0;k--){
+                if(number[k]!=2){
+                    Console.Write(number[k]);
+                }
+             }
+        }
+        Console.WriteLine("--------------------");
+    }
+
+
+int [] arr={1,2,3,4,10};
+Dec(arr);
+*/
+// Напишите программу, котора принимает на вход три числа и проверяет может ли существовать 
+//треугольник со сторонами такой длины.
+//Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.
+/*
+
+void Tringle (int a,int b,int c){
+    int sumA=b+c;
+    int sumB=a+c;
+    int sumC=a+b;
+    if (a<sumA&&b<sumB&&c<sumC){
+        Console.WriteLine("It is ok");
+
+    }
+    else{
+        Console.WriteLine("You can't create triangle with this length of sides");
+    }
+
+
+
+}
+
+
+Console.WriteLine("Input length of first side of tringle: ");
+int a=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input length of second side of tringle: ");
+int b=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input length of third side of tringle: ");
+int c=Convert.ToInt32(Console.ReadLine());
+
+Tringle(a,b,c);
+*/
+//Не используя рекурсию выведите n чисел Фибонначи
+//Первые два числа Фибоначчи
+/*
+void Fibonacci (int n){
+    int [] array=new int[n];
+    array[0]=0;
+    array[1]=1;
+
+    for (int i=2; i<array.Length;i++){
+        array[i]=array[i-1]+array[i-2];
+    }
+     for (int i=0; i<array.Length;i++){
+        Console.Write(array[i]+" ");
+     }
+}
+Console.WriteLine("Input number: ");
+int n=Convert.ToInt32(Console.ReadLine());
+Fibonacci(n);
+*/
---
+++ Task_41_43/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+/*
+void CountOfNumber (int[]arr){
+    int count =0;
+    for (int i=0; i<arr.Length;i++){
+        if(arr[i]>0){
+            count++;
+        }
+    }
+    Console.WriteLine($"You input {count} numbers more 0");
+    }
+
+int size=0;
+int[] array=new int[100];
+
+while(true){
+    Console.WriteLine("Input number or input end to break: ");
+    string input = Console.ReadLine();
+    if (input=="end"){
+        Console.WriteLine("You have finished input"); 
+        break;
+}
+    else{
+        int n=Convert.ToInt32(input);
+        array[size]=n;
+        size++;
+        
+        }
+    }
+
+CountOfNumber(array);
+*/
+
+//Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+//y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+/*
+void Equation (double k1,double b1,double k2,double b2){
+    double x=(b2-b1)/(k1-k2);
+    double y=k1*x+b1;
+    Console.WriteLine($"Point of intersection of two sraight lines is: ({x};{y})");
+    }
+
+Console.WriteLine("Two sraight lines are written by the equations:");
+Console.WriteLine("y = k1 * x + b1, y = k2 * x + b2");
+Console.WriteLine();
+Console.WriteLine("Input number k1: ");
+double k1=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number b1: ");
+double b1=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number k2: ");
+double k2=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number b2: ");
+double b2=Convert.ToInt32(Console.ReadLine());
+
+
+Equation(k1,b1,k2,b2);
+*/
